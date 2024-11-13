0b5aee598afa41565f3fc2785ac3b2681f11f31f
Alan Sanchez <sanchez1.alan1@gmail.com>
2024-11-13 14:48:10 -0700

    Finishing Touches
    Added some images, css files, and small touches for Testimonials, About-Us, a footer, a nice scrollbar, and etc.

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ app.get('/logout', (req, res) => {res.clearCookie('token'); console.log("User lo
 app.get('/login', (req, res) => res.render('login', { redirectTo: req.query.redirectTo || '/' }));
 app.get('/signup', (req, res) => res.render('signup', { redirectTo: req.query.redirectTo || '/' }));
 app.get('/create-pet', (req, res) => res.render('create-pet'));
+app.get('/about-us', (req, res) => res.render('about-us'));
+app.get('/testimonials', (req, res) => res.render('testimonials'));
 app.get('/admin', async (req, res) => {
     if (!req.user?.admin) return res.status(403).send('Access denied');
     const User = require('./models/users');
@@ -64,3 +66,13 @@ const startServer = async () => {
 };
 
 startServer();
+
+/* ENV CODE
+MONGOURI="YOUR CONNECTION STRING"
+PORT = "5000"
+JWT_SECRET= "YOUR SECRET PASSWORD"
+CLOUDINARY_CLOUD_NAME="YOUR CLOUDINARY CLOUD NAME"
+CLOUDINARY_API_KEY="YOUR CLOUDINARY API KEY"
+CLOUDINARY_API_KEY_SECRET="YOUR CLOUDINARY API KEY SECRET"
+EMAIL_USERNAME= "THE EMAIL YOU WANT TO SEND EMAILS"
+EMAIL_PASSWORD= "YOUR SPECIAL APP PASSWORD FOR YOUR EMAIL (NOT NORMAL PASSWORD)" */
